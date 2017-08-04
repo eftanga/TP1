d9cb8c493a56a399da9ba7cfaffc5c15b918da99
Enzo <eftanga@gmail.com>
2017-08-04 19:38:07 -0300

    commit 2

--- Ej1/Program.cs
+++ Ej1/Program.cs
@@ -11,7 +11,7 @@ namespace Ej1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo!!!");
-            Console.ReadLine();
+            Console.ReadLine();   
         }
     }
 }
